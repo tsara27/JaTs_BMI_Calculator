326d92ac7b451650b033c4428db27ab8ccc72117
Tsara Fatma L <tsara_tf@bitbucket.org>
2014-03-07 19:20:15 +0700

    Initial BMI app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+JaTsBmiCalculator::Application.config.secret_key_base = '51fd39c4c4b37a3c9af968aea5024c8b38d0ae4741225cbb25e2b2add3744f22422e5e5535e860057a77776015f0a4743aef8251011d2a434a273f153561fa07'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+JaTsBmiCalculator::Application.config.session_store :cookie_store, key: '_JaTs_BMI_Calculator_session'
